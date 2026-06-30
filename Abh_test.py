a5c329bf761ace23b3b54e25ecd97bd6e23974c2
Abhishek Nair <absk6789@gmail.com>
2026-06-30 18:19:21 +0530

    New changes for env

--- Abh_test.py
+++ Abh_test.py
@@ -1,2 +1,12 @@
 # Databricks notebook source
 print('hello new version')
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--catalog")
+parser.add_argument("--schema")
+
+args = parser.parse_args()
+
+print(args.catalog)
+print(args.schema)
